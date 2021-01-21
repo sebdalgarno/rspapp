1fdfef9381f7ef776acbacb7cba6046853aca8f7
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-01-20 18:59:08 -0800

    before adding componenets

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
 	import ExampleComponent from "../components/ExampleComponent.svelte";
+	import Navbar from "../components/Navbar.svelte";
+
 </script>
 
 <style>
@@ -7,6 +9,8 @@
 		@apply flex-1 flex flex-col items-center justify-center;
 	}
 </style>
+
+<Navbar></Navbar>
 
 <div class:centerer={true}> <!-- Here's how you could extract conditional class groups with @apply -->
 	<ExampleComponent
