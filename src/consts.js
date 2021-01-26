08fe9cceec907239d7a93fb6129f544003106b48
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-01-26 10:55:56 -0800

    bounds stay at better zoom

--- src/consts.js
+++ src/consts.js
@@ -1,16 +1,14 @@
-export const mapbox_key =
-  "pk.eyJ1Ijoibm9ydGhiZWFjaGNvbnN1bHRpbmciLCJhIjoiY2tld2ZkdXA1MmZ5cjJxcXZ4aWN5eWRteSJ9.GmGSqnuACz13roMMveTdsw";
+export const mapbox_key = "pk.eyJ1Ijoibm9ydGhiZWFjaGNvbnN1bHRpbmciLCJhIjoiY2tld2ZkdXA1MmZ5cjJxcXZ4aWN5eWRteSJ9.GmGSqnuACz13roMMveTdsw";
 
 export const tileset_logging = "mapbox://northbeachconsulting.6jbkd2ay";
 
-export const source  = ["logged_bc_zgZ45-68y3li"]
+export const source = ["logged_bc_zgZ45-68y3li"];
 
 export const source_layer = ["logged_simple5"];
 
-export const mapbox_style =
-  "mapbox://styles/northbeachconsulting/ckk9ybto8082e17p6xofk5mc2";
+export const mapbox_style = "mapbox://styles/northbeachconsulting/ckk9ybto8082e17p6xofk5mc2";
 
-  // dark
+// dark
 // export const mapbox_style =
 // "mapbox://styles/northbeachconsulting/ckkasz85604qh17nvxf01f46g"
 // export const tileset_base = "mapbox://northbeachconsulting.0q6r8ewi";
@@ -18,8 +16,8 @@ export const mapbox_style =
 export const coordinates = [-124.75, 54.77];
 
 export const bounds = [
-  [-134.45, 48.30],
-  [-114.88, 56.21],
+	[-136.92, 47.24],
+	[-113.83, 56.45],
 ];
 
 export const year_min = 1970;
