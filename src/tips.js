1c506eb3d504f10547765d520808ebf817cebdf3
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-01-26 10:32:16 -0800

    typo

--- src/tips.js
+++ src/tips.js
@@ -1,5 +1,5 @@
 export const tips_lh_year = "Press the Play button to view animation of logging history over time. You can also press the +/- buttons to navigate manually. As the year increases, the chart and map will add data corresponding to that year.";
 
-export const tips_lh_map = "The map can be zoomed and panned with your mouse or touchpad (scroll for zoom and click and drag for pan). At low zoom levels (i.e. province-wide) the data are shown with less detail. Zoom into a particular area of interest to see hmore detail.";
+export const tips_lh_map = "The map can be zoomed and panned with your mouse or touchpad (scroll for zoom and click and drag for pan). At low zoom levels (i.e. province-wide) the data are shown with less detail. Zoom into a particular area of interest to see more detail.";
 
 export const tips_forestlayers = "To begin, either move the slider or press the +/- buttons in the bottom left. This will increase/decrease years and show corresponding logged areas in the map and chart. The map can be zoomed and panned with your mouse (scroll for zoom and click and drag for pan). Higher resolution logged areas are shown as you zoom in. To view logged areas for an individual year, click the 'by year' button. To view second growth logging as a separate colour, click the '2nd growth' button.";
